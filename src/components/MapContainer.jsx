ce5ef4a03baa348ac349c58ed1c0d0ce42602074
seungwoo <136046385+walter230@users.noreply.github.com>
2026-01-14 15:27:03 +0900

    Update map style to light theme (Naver Map style)

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -23,8 +23,8 @@ const MapComponent = ({ data, onSelect, selectedId }) => {
             zoomControl={false}
         >
             <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
             />
 
             {data.map((loc) => {
